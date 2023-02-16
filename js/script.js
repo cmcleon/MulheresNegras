b57cfbf17a10daa5a6e452e434be184f42293ab2
cmcleon <candicem.leon@gmail.com>
2023-02-15 22:41:22 -0300

    :up: DOM

--- js/script.js
+++ js/script.js
@@ -64,18 +64,27 @@ function carregaDicionario() {
   
     };
 
-    for (var bio in biografias){
+    var content = document.getElementById("content");
+
+
+  for (var bio in biografias) {
+    content.innerHTML +=
+      '<div class="card">' +
+      '<img src="' +
+      biografias[bio].imagem +
+      '"/>' +
+      "<details>" +
+      "<summary>" +
+      biografias[bio].nome +
+      "</summary>" +
+      "<p>" +
+      biografias[bio].descricao +
+      "</p>" +
+      "<blockquote><q>" +
+      biografias[bio].citacao +
+      "</q></blockquote>" +
+      "</details></div>";
          
-    console.log(
-        (bio += " " +"Nome: " + 
-        biografias[bio].nome + 
-        " " + 
-        "Minibio: " + 
-        biografias[bio].descricao +
-        " "+
-        "Citação: " +
-        biografias[bio].citacao));
-    }
-  
   }
-  carregaDicionario();
+}
+    carregaDicionario();
